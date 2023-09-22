4d2781e6c2a643c59b835317c932b3a3c60b9c7e
AMBOT <144463605+AMBOTTEST@users.noreply.github.com>
2023-09-23 00:59:12 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("git clone https://github.com/AMBOTTEST/SatyamManagerbot && cd SatyamManagerbot && python3 -m Exon")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
