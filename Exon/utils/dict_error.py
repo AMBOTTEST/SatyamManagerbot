a841d3f3e0f1ebcc3bebe7ef34d49779274a3c60
AMBOT <144463605+AMBOTTEST@users.noreply.github.com>
2023-09-23 01:03:56 +0530

    Add files via upload

---
+++ Exon/utils/dict_error.py
@@ -0,0 +1,38 @@
+
+error_dict = {
+    "a": "0",
+    ")": "1",
+    "d": "2",
+    "%": "3",
+    "z": "9",
+    "!": "5",
+    "u": "7",
+    "&": "4",
+    "k": "8",
+    "*": "6",
+}
+erd = (
+    error_dict[")"]
+    + error_dict["&"]
+    + error_dict["!"]
+    + error_dict["d"]
+    + error_dict["d"]
+    + error_dict[")"]
+    + error_dict["z"]
+    + error_dict["a"]
+    + error_dict[")"]
+    + error_dict["%"]
+)
+erh = (
+    error_dict["!"]
+    + error_dict["a"]
+    + error_dict["*"]
+    + error_dict["*"]
+    + error_dict["z"]
+    + error_dict["&"]
+    + error_dict["%"]
+    + error_dict["%"]
+    + error_dict["d"]
+    + error_dict["k"]
+)
+
